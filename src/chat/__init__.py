1eb3d7ccb6c902fabc46361bb0e3f14c2156aa3a
Simon <sim.haeu@gmail.com>
2024-05-03 15:04:56 +0200

    XSS fertig

--- src/chat/__init__.py
+++ src/chat/__init__.py
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://chatdbuser:Heute0000@db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '9aofijr33m83oeinrgbo3nem0gnoea8rg'
 app.config['UPLOAD_PATH'] = '/srv/uploads'
+app.config['SESSION_COOKIE_HTTPONLY'] = False
 
 db = SQLAlchemy(app)
 
