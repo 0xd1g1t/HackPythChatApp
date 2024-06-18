a258d1fb9af066a0d96ee18917615fbf14ec3e64
Simon <sim.haeu@gmail.com>
2024-06-18 08:09:32 +0200

    asdf

--- src/chat/__init__.py
+++ src/chat/__init__.py
@@ -7,7 +7,7 @@ UPLOAD_PATH = '/srv/uploads'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://chatdbuser:Heute0000@db/chatdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '9aofijr33m83oeinrgbo3nem0gnoea8rg'
+app.config['SECRET_KEY'] = 'b9399f21060d4b5fcb6d3cf5fea8de'
 app.config['UPLOAD_PATH'] = '/srv/uploads'
 app.config['SESSION_COOKIE_HTTPONLY'] = False
 
